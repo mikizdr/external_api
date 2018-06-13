0589eac38894aa8a4ac8f69f57f162a431641609
Мирослав Здравковић <miroslav@fimanager.com>
2018-06-13 17:57:06 +0200

    routes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,62 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
+
+/*
+Get filters
+GET /activities/filters
+
+Returns a list of the available filters this club has configured for activities
+ 
+Get activities
+GET /activities
+URL parameters:
+lower_date (optional): the minimum start date 
+upper_date (optional): the maximum start date
+filter_ids (optional): comma separated list of available filters 
+
+Returns a list of available activities from your club
+
+Get activity
+GET /activity/<ID>
+
+Returns a single activity
+
+Register person for activity
+
+POST /registration 
+POST data:
+activity_id (required)
+first_name (required)
+last_name (required)
+email (optional)
+phone (optional)
+gender (optional)
+
+Registers a person for the given activity id. Returns a unique ID for this registration
+
+Unregister person
+DELETE /registration/<REGISTRATION_ID>
+
+Removes a registration 
+
+Mark Present
+PUT /registration/<ID>/presence
+
+Mark the person ‘present’ for the given registration (‘check-in’)
+
+Mark Absent
+DELETE /registration/<ID>/presence
+
+Mark the person ‘absent’ for the given registration (cancel ‘check-in’)
+
+*/
