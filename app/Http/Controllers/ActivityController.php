1517c3ffd4154d509b557d2a4b8485a2308a73b2
Мирослав Здравковић <miroslav@fimanager.com>
2018-06-14 12:44:03 +0200

    Activity model and ActivityController with route model binding

---
+++ app/Http/Controllers/ActivityController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Activity;
+
+class ActivityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Activity::paginate(3);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Activity  $activity
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Activity $activity)
+    {
+        return $activity;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  Activity  $activity
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Activity $activity)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  Activity  $activity
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Activity $activity)
+    {
+        //
+    }
+}
