4a19eea0a90c797055508cfb6a11375c2b2d990b
miroslavmgr <miroslav@fitmanager.com>
2018-06-25 14:20:05 +0200

    Role model, controller, migration, factory, request, seeder

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::truncate();
+
+        $roles = ['member', 'trainer', 'owner'];
+
+        foreach ($roles as $key => $role) {
+            Role::create([
+                'role' => $role
+            ]);
+        }
+    }
+}
