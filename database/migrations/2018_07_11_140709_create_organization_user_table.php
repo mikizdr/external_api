54efb784f7b97dd5d3dc79f9d63662244965e924
miroslavmgr <miroslav@fitmanager.com>
2018-07-18 14:51:06 +0200

    organization_user table migration

---
+++ database/migrations/2018_07_11_140709_create_organization_user_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrganizationUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organization_user', function (Blueprint $table) {
+            $table->integer('creator_id');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->integer('user_id');
+            $table->integer('club_id');
+            $table->integer('role_id');
+            $table->string('status');
+            $table->string('approval_date')->nullable();
+            $table->string('start_date')->nullable();
+            $table->string('end_date')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->primary(['user_id', 'club_id', 'role_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organization_user');
+    }
+}
