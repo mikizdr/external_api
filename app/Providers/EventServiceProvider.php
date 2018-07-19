e9d6c87f152414dd0e63e41823c914f18434e3c7
Мирослав Здравковић <miroslav@fimanager.com>
2018-07-19 13:32:41 +0200

    php artisan event:generate

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -13,8 +13,9 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\Event' => [
-            'App\Listeners\EventListener',
+        'App\Events\CreateUserEvent' => [
+            'App\Listeners\CreateUserEventListener',
+            'App\Listeners\AttachOrganizationEventListener',
         ],
     ];
 
