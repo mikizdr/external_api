12e2a44881cb00c311fac5b4e76f276f024ecd61
Мирослав Здравковић <miroslav@fimanager.com>
2018-07-26 15:21:08 +0200

    Exceptions handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,8 +4,10 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 
 class Handler extends ExceptionHandler
 {
@@ -51,11 +53,19 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        if ($exception instanceof NotFoundHttpException) {
-            return response()->json([
-                'error' => 'Resource not found.'
-            ], Response::HTTP_NOT_FOUND);
-        }
+      if ($exception instanceof ModelNotFoundException) {
+        return response()->json([
+            'error' => 'Resource not found.'
+        ], Response::HTTP_NOT_FOUND);
+      } elseif ($exception instanceof NotFoundHttpException) {
+          return response()->json([
+              'error' => 'Bad request or incorect request parameters.'
+          ], Response::HTTP_NOT_FOUND);
+      } elseif ($exception instanceof QueryException) {
+          return response()->json([
+              'error' => 'The trequired data don\'t exist in the db.'
+          ], Response::HTTP_NOT_FOUND);
+      }
 
         return parent::render($request, $exception);
     }
